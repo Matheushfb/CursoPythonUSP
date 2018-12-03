1a3780e472ea2b34d3b13c7c8babe7fd7ce4ea0c
Matheushfb <11398467+Matheushfb@users.noreply.github.com>
2018-12-03 15:27:16 -0200

    Update areaquadrado.py

--- areaquadrado.py
+++ areaquadrado.py
@@ -1,4 +1,4 @@
-lado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-perimetro = lado * 4
-area = lado ** 2
-print("perímetro:",perimetro,"-","área:",area)
+lado = int(input("Digite o valor correspondente ao lado de um quadrado: ")) #Entrada do valor do lado de um quadrado
+perimetro = lado * 4  #Calculo do perimetro  
+area = lado ** 2 #Calculo da área
+print("perímetro:",perimetro,"-","área:",area) # Printando os resultados
