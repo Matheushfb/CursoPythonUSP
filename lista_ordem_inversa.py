a0e0b12dfa98d0354d87feedd061c8400f26802d
Matheushfb <11398467+Matheushfb@users.noreply.github.com>
2018-12-03 22:26:16 -0200

    Update lista_ordem_inversa.py

--- lista_ordem_inversa.py
+++ lista_ordem_inversa.py
@@ -1,12 +1,12 @@
-lista = []
-pos = 0
-numero = 1 
-while numero != 0:
-    numero = int(input("digite um numero: "))
-    lista.append(numero)
-    pos = pos + 1
+lista = [] # Criando lista vazia
+numero = 1  
 
-tamanho = len(lista) - 1
-while tamanho >= 0:
-    print(lista[tamanho],end = "")
-    tamanho = tamanho - 1 
+while numero != 0: # Laço se repete enquanto nosso input for diferente de 0, esse é o motivo da variavel numero ser inicializada com o valor 1
+    numero = int(input("digite um numero: "))  # Recebe um numero 
+    lista.append(numero) # Adiciona o numero a lista  
+
+## Printando invertido ##
+tamanho = len(lista) - 1  # Primeiro geramos uma variavel com o valor do comprimento da lista para ser decrementada 
+while tamanho >= 0: # Enquanto essa lista não for percorrida até seu primeiro elemento o loop continua
+    print(lista[tamanho],end = "")  # Printando a posição da variavel
+    tamanho = tamanho - 1  # decrementando a variavél ate que chegue a 0
