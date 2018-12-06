63e5ea80318787f030f64e1032ad4525c683c0ea
Matheushfb <11398467+Matheushfb@users.noreply.github.com>
2018-12-06 16:15:22 -0200

    Adição de comentário no código

--- crescente.py
+++ crescente.py
@@ -1,8 +1,12 @@
+## Algoritmo deve receber 3 numeros e veerificar se estão em ordem crescente ou não ##
+
+#Entrada das variáveis
 n = int(input("Insira o primeiro número: "))
 n2 = int(input("Insira o segundo número: "))
 n3 = int(input("Insira o terceiro número: "))
 
-if(n <= n2):
+# Verificação das variaveis 
+if(n <= n2):  
     if(n2 <= n3):
         print("crescente")
     else:
